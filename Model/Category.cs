c2a3698002dacde2352f8d4419eee0edb7390e70
claraastenz <castenz18@gmail.com>
2024-05-14 17:34:30 -0500

    [1-04] added appsettings.json Scaffold database context and entity classes

---
+++ Model/Category.cs
@@ -0,0 +1,22 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Model
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "YO - Enter the name!")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
